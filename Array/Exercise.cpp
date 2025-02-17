51bdf071bd00f56610ad2a4fb5e2e90b8eb54e1b
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-02-17 16:46:55 +0330

    Add insert methode

--- Array/Exercise.cpp
+++ Array/Exercise.cpp
@@ -2,10 +2,12 @@
 class Array{
 
   private:
+    int count = 0;
     int length;
     int *items;
 
   public:
+
     Array(int length)
     { 
         this->length = length;
@@ -14,11 +16,33 @@ class Array{
 
     void print()
     {
-        for(int i = 0; i < length;i++)
+        for(int i = 0; i < count;i++)
         {
             std::cout<<items[i]<<std::endl;
         }
     }
+    void insert(int item)
+    {
+        //If the array is full, resize it
+
+        if (length == count)
+        {
+          //Creat a new array (twice the size)
+          int *newItems = new int[count * 2];
+          //Copy all the exsiting items
+          for (int i = 0; i < count;i++)
+          {
+            newItems[i] = items[i];
+          }
+          //Set "items" to this new array
+          items = newItems;
+
+        }
+        //Add the new item at the end
+        items[count++] = item;
+    }
+
+
 
 };
 
@@ -29,6 +53,10 @@ int main()
 {
 
     Array numbers(3);
+    numbers.insert(2);
+    numbers.insert(3);
+    numbers.insert(2);
+    numbers.insert(3);
     numbers.print();
 
     return 0;
