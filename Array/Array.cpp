9fc18db90b6d6b556d9417de498c476240a10a60
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-02-17 15:56:15 +0330

    Commit

--- Array/Array.cpp
+++ Array/Array.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 int main() {
-<<<<<<< HEAD
-	std::cout << "hello world" << std::endl;
-    std::cin.get();
-=======
+
 	int *numbers = new int[3] {1,2,3};
 	std::cout<<numbers[0];
 
->>>>>>> Array
 	return 0;
 }
