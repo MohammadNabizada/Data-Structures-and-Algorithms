361d5656725809883ec4547827c9c13aa3203043
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-02-22 21:00:35 +0330

    Add isBalanced class

--- Stacks/Exercise.cpp
+++ Stacks/Exercise.cpp
@@ -9,8 +9,6 @@ class StringReverser{
 
     if(input == std::string("null"))
      throw std::invalid_argument("you give me a bad argument");
-
-
      std::stack<char> stack;
 
      for(char ch : input)
@@ -29,11 +27,40 @@ class StringReverser{
 
 };
 
+class Expression{
+  public:
+   bool isBalanced(std::string input){
+    std::stack<char> stack;
 
+    for(char ch : input)
+    {
+      if(ch == '(' || ch == '[' || ch == '<' || ch == '{')
+        stack.push(ch);
+      
+      if(ch == ')'  || ch == ']' || ch == '>' || ch =='}'){
+        if (stack.empty()) return false;
+        char top = stack.top();
+        if(
+          (ch == ')' && top != '(') ||
+           (ch == '>' && top != '<') ||
+           (ch == ']' && top != '[') ||
+           (ch == '}' && top != '{')
+        ) return false;
+        stack.pop();
+      }
+    }
+    return stack.empty();
+   }
+};
 int main()
 {
     std::string str = "abcd"; 
     StringReverser *reverser = new StringReverser();
     std::cout<<reverser->reverse(str);
+
+    std::cout<<std::endl;
+    Expression *exp = new Expression();
+    std::string str2 = "(1+2)";
+    std::cout<<exp->isBalanced(str2);
     return 0;
 }
