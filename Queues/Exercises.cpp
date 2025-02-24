98fadff5f246eb27d590c7f3cbede8cdf0847a66
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-02-24 15:26:10 +0330

    Add reverse method to inheritance of queue class in cpp stl library

---
+++ Queues/Exercises.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+//Adding a new method to queue built-in class in cpp
+template <typename T>
+class CustomQueue : public std::queue<T>
+{
+  public:
+    void reverse()
+    {
+        std::stack<T> stack;
+        while(!this->empty())
+        {
+            stack.push(this->front());
+            this->pop();
+        }
+        while(!stack.empty())
+        {
+            int pu = stack.top();
+            this->push(pu);
+            stack.pop();
+        }
+    }
+    void printQueue() {
+        std::queue<T> tempQueue = *this;
+        while (!tempQueue.empty()) {
+            std::cout << tempQueue.front() << " ";
+            tempQueue.pop();
+        }
+        std::cout << std::endl;
+    }
+};
+
+
+int main()
+{
+    CustomQueue<int> queue;
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
+    queue.push(40);
+    queue.push(50);
+
+    std::cout << "Queue elements: ";
+    queue.printQueue();
+    queue.reverse(); 
+    std::cout << "Queue elements: ";
+    queue.printQueue();
+    return 0;
+}
