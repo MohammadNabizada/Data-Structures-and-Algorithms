58b085d29b0c7023541d811ea682e3ab3ef6fff1
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-02-26 17:16:49 +0330

    Add a priority queue of std library

--- Queues/Queues.cpp
+++ Queues/Queues.cpp
@@ -8,6 +8,21 @@ int main()
     queue.push(30);
     int top = queue.front();
     std::cout<<top;
+    //make a priority queue
+    std::cout<<std::endl;
+    std::priority_queue<int> pQueue;
+    
+    pQueue.push(1);
+    pQueue.push(3);
+    pQueue.push(6);
+    pQueue.push(2);
+
+    while(!pQueue.empty())
+    {
+        std::cout<<pQueue.top()<<std::endl;
+        pQueue.pop();
+    }
+
 
 
     return 0;
