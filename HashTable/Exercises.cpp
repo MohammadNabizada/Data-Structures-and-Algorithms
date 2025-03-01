6871e0dd419238893c26e155161b9c1094b78272
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-03-01 09:08:16 +0330

    Deploy firstNoneRepeatedCharacterClass

---
+++ HashTable/Exercises.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unordered_map>
+class firstNonerepeat
+{
+   private:
+    std::unordered_map<char,int> map;
+    std::string str;
+   public:
+   firstNonerepeat(std::string str)
+   {
+    this->str = str;
+   }
+    char noneRepeated()
+    {
+        for(const auto& ch : str)
+        {
+            map[ch]++;
+        }   
+        for(const auto& ch :str)
+        {
+            if(map[ch] == 1)
+              return ch;
+        }
+        return '\0';
+    }
+
+
+
+
+};
+
+int main()
+{
+
+
+
+
+
+    return 0;
+}
