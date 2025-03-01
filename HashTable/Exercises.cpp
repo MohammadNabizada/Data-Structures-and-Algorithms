a3c9ea359bc8c5d15358259ee4e037c344031b0b
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-03-01 09:53:19 +0330

    Add firstrepeated method

--- HashTable/Exercises.cpp
+++ HashTable/Exercises.cpp
@@ -3,7 +3,6 @@
 class firstNonerepeat
 {
    private:
-    std::unordered_map<char,int> map;
     std::string str;
    public:
    firstNonerepeat(std::string str)
@@ -12,6 +11,7 @@ class firstNonerepeat
    }
     char noneRepeated()
     {
+        std::unordered_map<char,int> map;
         for(const auto& ch : str)
         {
             map[ch]++;
@@ -23,6 +23,22 @@ class firstNonerepeat
         }
         return '\0';
     }
+    char firstRepeatedchar()
+    {
+        std::unordered_map<char,int> map;
+        for(const auto& ch : str)
+        {
+          map[ch]++;
+        }
+
+        for(const auto& ch : str)
+        {
+            if(map[ch] > 1)
+             return ch;
+        }
+
+        return '\0';
+    }
 
 };
 
@@ -32,6 +48,8 @@ int main()
 
     firstNonerepeat noneRepeated("a green apple.");
     std::cout<<noneRepeated.noneRepeated();
+    std::cout<<std::endl;
+    std::cout<<noneRepeated.firstRepeatedchar();
 
 
     return 0;
