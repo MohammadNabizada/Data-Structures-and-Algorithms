45b4109c9b80ee25b2351b199871677eda573347
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-03-04 13:48:13 +0330

    Add find method

--- Binary trees/Exercise.cpp
+++ Binary trees/Exercise.cpp
@@ -52,9 +52,31 @@ class Tree
       } 
     }
 
+    int find(int value)
+    {
+        Node* current = root;
+        if(root == nullptr)
+          throw std::invalid_argument("tree is empty");
+        
+        while(current->value != value)
+        {
+          if(value < current->value)
+          {
+            if(current->leftchild == nullptr){
+                return -1;
+            }
+            current = current->leftchild;
+          }
+          else{
+            if(current->rightchild == nullptr){
+                return -1;
+            }
+            current = current->rightchild;
+          }
+          }
 
-
-
+          return 1;
+        }
 };
 
 int main()
@@ -69,6 +91,7 @@ int main()
     tree->insert(8);
     tree->insert(6);
     tree->insert(3);
+    std::cout<<"find 3:"<<tree->find(3);
     std::cout<<"Done";
 
 
