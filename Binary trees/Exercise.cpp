fad4a6865b0180b940fa193b142802a994226d7b
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-03-04 13:53:09 +0330

    Refactor find method

--- Binary trees/Exercise.cpp
+++ Binary trees/Exercise.cpp
@@ -52,30 +52,20 @@ class Tree
       } 
     }
 
-    int find(int value)
+    bool find(int value)
     {
         Node* current = root;
-        if(root == nullptr)
-          throw std::invalid_argument("tree is empty");
-        
-        while(current->value != value)
+        while(current != nullptr)
         {
           if(value < current->value)
-          {
-            if(current->leftchild == nullptr){
-                return -1;
-            }
             current = current->leftchild;
-          }
-          else{
-            if(current->rightchild == nullptr){
-                return -1;
-            }
+          else if(value > current->value)
             current = current->rightchild;
-          }
-          }
+          else 
+            return true;
+        }
 
-          return 1;
+          return false;
         }
 };
 
@@ -91,8 +81,7 @@ int main()
     tree->insert(8);
     tree->insert(6);
     tree->insert(3);
-    std::cout<<"find 3:"<<tree->find(3);
-    std::cout<<"Done";
+    std::cout<<"find 3:"<<tree->find(17);
 
 
 
