dc0280b69fe7b91392e92a5ac933cdace7f1f2c9
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-03-09 10:51:18 +0330

    Refactor the code

--- Avl trees/Avl_tree.cpp
+++ Avl trees/Avl_tree.cpp
@@ -2,100 +2,98 @@
 #include <string.h>
 using namespace std;
 
-
 class Avltree
 {
-  private:
-    class Node{
-         public :
-         int value;
-         Node* leftchild;
-         Node* rightchild;
-         int height = 0;
-         string isBalancedfactor;
-
-         Node(int value)
-         {
-            this->value = value;
-            leftchild = nullptr;
-            rightchild = nullptr;
-            isBalancedfactor = "Balanced";
-         }
-    };
-
-    Node *root = nullptr;
-  Node* insert(int value,Node* root)
+private:
+  class Node
   {
-    if(root == nullptr)
-      return new Node(value);
-    if(value < root->value)
-    {
-      root->leftchild = insert(value,root->leftchild);
+  public:
+    int value;
+    Node *leftchild;
+    Node *rightchild;
+    int height = 0;
+    string isBalancedfactor;
 
-    }else{
-      root->rightchild = insert(value,root->rightchild);
+    Node(int value)
+    {
+      this->value = value;
+      leftchild = nullptr;
+      rightchild = nullptr;
+      isBalancedfactor = "Balanced";
     }
-    root->height = max(height(root->leftchild),height(root->rightchild)) + 1;
-    if(isLeftHeavy(root))
+  };
+
+  Node *root = nullptr;
+  Node *insert(int value, Node *root)
+  {
+    if (root == nullptr)
+      return new Node(value);
+    if (value < root->value)
+    {
+      root->leftchild = insert(value, root->leftchild);
+    }
+    else
+    {
+      root->rightchild = insert(value, root->rightchild);
+    }
+    root->height = max(height(root->leftchild), height(root->rightchild)) + 1;
+    if (isLeftHeavy(root))
       root->isBalancedfactor = "leftHeavy";
-    else if(isRighttHeavy(root))
-      root->isBalancedfactor= "rightHeavy";
-       
+    else if (isRighttHeavy(root))
+      root->isBalancedfactor = "rightHeavy";
+    
+    
     return root;
   }
   bool isleaf(Node *node)
   {
-     return node->leftchild == nullptr && node->rightchild == nullptr;
+    return node->leftchild == nullptr && node->rightchild == nullptr;
   }
   int height(Node *node)
   {
-   return (node == nullptr) ? -1 : node->height;
+    return (node == nullptr) ? -1 : node->height;
   }
-   bool isLeftHeavy(Node* node)
-   {
+  bool isLeftHeavy(Node *node)
+  {
     return Balancefactor(node) > 1;
-   }
-   bool isRighttHeavy(Node* node)
-   {
+  }
+  bool isRighttHeavy(Node *node)
+  {
     return Balancefactor(node) < -1;
-   }
+  }
 
-   int Balancefactor(Node *node)
-   {
+  int Balancefactor(Node *node)
+  {
     return (node == nullptr) ? 0 : height(node->leftchild) - height(node->rightchild);
-   }
-   
+  }
 
 
-  public:
+
+
+public:
   void insert(int value)
   {
-    root = insert(value,root);
+    root = insert(value, root);
   }
-
-
 
 
 
 };
 
-
-
 int main()
 {
 
-    Avltree *tree = new Avltree();
-    tree->insert(7);
-    tree->insert(4);
-    tree->insert(9);
-    tree->insert(1);
-    tree->insert(6);
-    tree->insert(8);
-    tree->insert(15);
-    tree->insert(14);
-    tree->insert(13);
-    tree->insert(2);
+  Avltree *tree = new Avltree();
+  tree->insert(7);
+  tree->insert(4);
+  tree->insert(9);
+  tree->insert(1);
+  tree->insert(6);
+  tree->insert(8);
+  tree->insert(15);
+  tree->insert(14);
+  tree->insert(13);
+  tree->insert(2);
 
-
-    return 0;
+  return 0;
 }
