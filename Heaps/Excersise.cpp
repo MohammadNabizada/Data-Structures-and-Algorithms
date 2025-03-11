ba429387b043c7ded9c83c13288e649f9a90745e
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-03-11 10:53:45 +0330

    test the insert method

--- Heaps/Excersise.cpp
+++ Heaps/Excersise.cpp
@@ -61,16 +61,13 @@ class Heap
 
 int main()
 {
-   Heap heap;
-   heap.insert(15);
-   heap.insert(12); 
-   heap.insert(9); 
-   heap.insert(8); 
-   heap.insert(10); 
-   heap.insert(3); 
-   heap.insert(4); 
-   heap.insert(1);
-   heap.insert(24); 
+   Heap *heap = new Heap();
+   heap->insert(10);
+   heap->insert(5);
+   heap->insert(17);
+   heap->insert(4);
+   heap->insert(22);
+   delete heap; 
  cout<<"done";
 
 
