55443ff7819082bade2645d716507d39b9fef70f
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-03-26 15:39:22 +0330

    Add print method

--- Undirected_graphs/Undirected_graphs.cpp
+++ Undirected_graphs/Undirected_graphs.cpp
@@ -19,11 +19,11 @@ class wightedGraph{
       Node* to;
       int weight;
 
-      Edge(weight): weight(weight){}
+      Edge(weight): weight(weight), from{nullptr}, to{nullptr}{}
 
     };
    unordered_map<string,Node*> nodes;
-   unordered_map<Node*,list<Node*>> adjacancyList;
+   unordered_map<Node*,list<Edge*>> adjacancyList;
 
    public:
    void addNode(string label)
@@ -31,11 +31,39 @@ class wightedGraph{
     if(nodes.find(label) == node.end()){
     Node* newNode = new Node(label);
     nodes[label] = newNode;
-    adjacancyList[newNode] = list<Node*>();
+    adjacancyList[newNode] = list<Edge*>();
     }
    }
 
-   
+   void addEdge(string from,string to, int weight)
+   {
+    auto fromKey = nodes.find(from);
+    auto tokey = nodes.find(to);
+    if(fromKey == nodes.end() || toKey == nodes.end())
+       return;
+       
+    Node* fromNode = fromKey->second;
+    Node* toNode = tokey->second;
+
+
+    Edge *edge = new Edge(fromNode,toNode,weight);
+    adjacancyList[fromNode].push_back(edge);
+    adjacancyList[toNode].push_back(edge);
+
+   }
+
+
+   void printAdjacancyList()
+   {
+    cout << "Adjacancy List";
+    for(auto& pair : adjacancyList)
+    {
+      Node* node = pair.first;
+
+      for(auto &edge : adjacancyList[node])
+        cout<<"["<<edge->from->label << "]" << "is Connected to" <<"[" edge->to << "]"<< "with weight" << edge->weight;
+    }
+   }
 
 };
 
