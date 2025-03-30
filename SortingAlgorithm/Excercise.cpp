1e76de5a59bbe8860dc5ebfc9cddf00ac1e3f607
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-03-30 16:25:58 +0330

    Reimplement bubble sort

--- SortingAlgorithm/Excercise.cpp
+++ SortingAlgorithm/Excercise.cpp
@@ -6,29 +6,35 @@ class Bublesort
 {
   private:
    int length;
+   void swap(int array[],int index1,int index2)
+   {
+     int temp = array[index1];
+     array[index1] = array[index2];
+     array[index2] = temp;
+   }
   public:
 
   Bublesort(int length) : length(length){}
+
+
   void sort(int array[])
   {
-    int temp;
-     for(int i = 0 ; i < length;i++)
-     {
-        for(int j = i+1; j < length;j++)
-        {
-            if(array[j] < array[i])
-            {
-              temp = array[j];
-              array[j] = array[i];
-              array[i] = temp;
-            }
-        }
-     }
+   for(int i = 0; i < length;i++)
+   {
+    for(int j = 1;j < length;j++)
+    {
+        if(array[j] < array[j-1])
+          {
+            swap(array,j,j-1);
+          }
+    }
+   }
 
-     for(int i =0; i < length;i++)
-     {
-        cout<<array[i];
-     }
+
+   for(int i =0; i < length;i++)
+   {
+    cout<< array[i];
+   }
   }
 
 
