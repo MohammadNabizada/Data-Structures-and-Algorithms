2824e6895a42e70bd1247427a3587666dccf4e96
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-03-30 17:01:57 +0330

    Implement selection sort

--- SortingAlgorithm/Excercise.cpp
+++ SortingAlgorithm/Excercise.cpp
@@ -40,6 +40,35 @@ class Bublesort
 
 };
 
+class SelectionSort{
+  private:
+  int length;
+  void swap(int array[],int index1,int index2)
+  {
+    int temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+  }
+
+  public:
+
+  SelectionSort(int length): length(length){}
+  void sort(int array[])
+  {
+   for(int i = 0;i < length;i++)
+   {
+   for(int j = i+1;j< length;j++)
+   {
+    if(array[j] < array[i])
+      swap(array,i,j);
+   }
+   }
+
+  }
+
+
+};
+
 
 
 
@@ -54,5 +83,14 @@ int main()
     {
      cout<< array[i];
     }
+    int array2[5] = {1,4,2,7,3};
+    SelectionSort selectionsort(5);
+    selectionsort.sort(array2);
+
+    for(int i =0; i < 5;i++)
+    {
+     cout<< array2[i];
+    }
+
     return 0;
 }
