7a397e43662cb331e3f2eb30682b68958e7cf5b1
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-03-30 15:59:56 +0330

    Bubble sort implementation

--- SortingAlgorithm/Excercise.cpp
+++ SortingAlgorithm/Excercise.cpp
@@ -1,10 +1,50 @@
-#inlcude <iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
+
+class Bublesort
+{
+  private:
+   int length;
+  public:
+
+  Bublesort(int length) : length(length){}
+  void sort(int array[])
+  {
+    int temp;
+     for(int i = 0 ; i < length;i++)
+     {
+        for(int j = i+1; j < length;j++)
+        {
+            if(array[j] < array[i])
+            {
+              temp = array[j];
+              array[j] = array[i];
+              array[i] = temp;
+            }
+        }
+     }
+
+     for(int i =0; i < length;i++)
+     {
+        cout<<array[i];
+     }
+  }
+
+
+
+};
+
+
+
 
 int main()
 {
 
+    int array[5] = {1,4,2,7,3};
 
+    Bublesort bublesort(5);
+    bublesort.sort(array);
 
 
 
