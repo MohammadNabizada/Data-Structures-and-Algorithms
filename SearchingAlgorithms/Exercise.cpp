471868426252da5917c59b0c498a007e8f403e71
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-04-06 21:20:12 +0330

    implement Binarysearch for finding the index

--- SearchingAlgorithms/Exercise.cpp
+++ SearchingAlgorithms/Exercise.cpp
@@ -3,6 +3,26 @@ using namespace std;
 class Search
 {
 
+private:
+
+
+int binarySearchRec(int arr[],int size,int target,int left,int right)
+{ 
+   int middle = (left + right) /2;
+   
+   if(right < left)
+     return -1;
+
+   if(arr[middle] == target)
+     return middle;
+
+   if(target < arr[middle])
+     return binarySearchRec(arr,size,target,left,middle-1);
+    
+   return binarySearchRec(arr,size,target,middle+1,right);
+}
+
+
 public:
 
    
@@ -10,13 +30,22 @@ public:
     {
         for(int i = 0; i < size;i++)
         {
-            if(arr[i] == item){
+            if(arr[i] == item)
+            {
               cout<<item<<" is in this array and the index is :"<<i;
               return;
             }
         }
-      cout<<item<<"is not found";
+        cout<<item<<"is not found";
     }
+
+    int binarySearchRec(int arr[],int size,int target)
+    {
+         return binarySearchRec(arr,size,target,0,size - 1);
+    }
+   
+
+    
 };
 
 
@@ -30,5 +59,10 @@ int main()
     Search search;
     search.linearSearch(1,5,array);
 
+    cout<<endl;
+    int array2[5] = {1,2,3,5,7};
+     
+    cout << "seraching for index of 3"<<endl;
+    cout<< search.binarySearchRec(array2,5,3);
     return 0;
 }
